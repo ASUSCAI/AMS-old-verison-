d0ea23551ae4501f8b7274361746fdcbfc9926e8
rtwoo <rtwoo@asu.edu>
2022-10-14 23:57:47 -0700

    Rough prototype

--- src/App.java
+++ src/App.java
@@ -1,11 +1,49 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.python.util.PythonInterpreter;
+
 public class App {
+
+    private static int count = 3;
+
     public static void main(String[] args) {
+        Statement statement = sqlTest();
+        try {
+            cardReadTest(statement);
+        } catch (IOException | InterruptedException | SQLException e) {
+            e.printStackTrace();
+        }
+        jythonTest();
+    }
+
+    private static void cardReadTest(Statement statement) throws IOException, InterruptedException, SQLException {
+        RandomAccessFile in = new RandomAccessFile("ids.txt", "r");
+        String line;
+        while (true) {
+            if ((line = in.readLine()) != null && line.length() > 0) {
+                System.out.println(line.trim());
+                statement.executeUpdate("INSERT INTO Students VALUES(" + count++ + ", 'John Doe')");
+                // TODO: pull student name and other data from a mock PeopleSoft database
+            } else {
+                Thread.sleep(2000); // check file every 2 sec
+            }
+        }
+    }
+
+    private static void jythonTest() {
+        try (PythonInterpreter pyInterp = new PythonInterpreter()) {
+            // pyInterp.exec("from canvasapi import Canvas");
+            pyInterp.exec("print('Hello Python World!')");
+        }
+    }
+
+    private static Statement sqlTest() {
         Connection connection = null;
         try {
             // create a database connection
@@ -23,18 +61,20 @@ public class App {
                 System.out.println("name: " + rs.getString("name"));
                 System.out.println("id: " + rs.getInt("id"));
             }
+            return statement;
         } catch (SQLException e) {
             // if the error message is "out of memory",
             // it probably means no database file is found
             System.err.println(e.getMessage());
         } finally {
-            try {
-                if (connection != null)
-                    connection.close();
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e.getMessage());
-            }
+            // try {
+            //     if (connection != null)
+            //         connection.close();
+            // } catch (SQLException e) {
+            //     // connection close failed.
+            //     System.err.println(e.getMessage());
+            // }
         }
+        return null;
     }
 }
