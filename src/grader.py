c4be237618424b9e4ee68a5e294dd7219dc98b22
rtwoo <rtwoo@asu.edu>
2022-10-15 17:53:52 -0700

    begin grader implementation

--- src/grader.py
+++ src/grader.py
@@ -1,13 +1,10 @@
 from calendar import weekday
 import peoplesoft
+import canvasapi
 
 
 def grade():
-    # start, end = peoplesoft.getTimes(57271)
-    # print(start, end)
-
-    nums = peoplesoft.getCanvasNums()
-    for num in nums:
-        if weekday() in peoplesoft.getDays(num):
-            start, end = peoplesoft.getTimes(num)
+    for courseID in peoplesoft.getCanvasNums():
+        if weekday() in peoplesoft.getDays(courseID):
+            start, end = peoplesoft.getTimes(courseID)
 grade()
