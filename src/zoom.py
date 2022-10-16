fdea0206f1e5c1f33dda6988579baf41e869e375
raoanmol <113382112+raoanmol@users.noreply.github.com>
2022-10-16 10:57:48 -0700

    Update zoom.py
    fixed error when student id start and end time does not exist

--- src/zoom.py
+++ src/zoom.py
@@ -6,33 +6,12 @@ identityData = pd.read_csv("canvasintel.csv")
 zoomData = pd.read_csv("participants_88187808697.csv")
 merged_data = identityData.merge(zoomData, on=["Name (Original Name)"]).set_index('asuid')
 
-def getId():
-    y = identityData['asuid'].tolist()
-    return y
-
-
-def startTime(studentID):
-    y = str(merged_data.at[studentID, 'Join Time'])
-    x = str(date.datetime.now().date()) + "T" + y[11:16] + ":00"
-    z = date.datetime.fromisoformat(x)
-    unixStartTime = int(time.mktime(z.timetuple()))
-    return unixStartTime
-
-
-
-def endTime(studentID):
-    y = str(merged_data.at[studentID, 'Leave Time'])
-    x = str(date.datetime.now().date()) + "T" + y[11:16] + ":00"
-    z = date.datetime.fromisoformat(x)
-    unixEndTime = int(time.mktime(z.timetuple()))
-    return unixEndTime
-
 
 def exists(studentID):
     counter = 0
     y = merged_data.index.tolist()
     for id in y:
-        if(studentID == id):
+        if studentID == id:
             counter += 1
 
     if counter == 1:
@@ -40,3 +19,30 @@ def exists(studentID):
 
     else:
         return False
+    
+
+def getId():
+    y = identityData['asuid'].tolist()
+    return y
+
+
+def startTime(studentID):
+    if exists(studentID):
+        y = str(merged_data.at[studentID, 'Join Time'])
+        x = str(date.datetime.now().date()) + "T" + y[11:16] + ":00"
+        z = date.datetime.fromisoformat(x)
+        unixStartTime = int(time.mktime(z.timetuple()))
+        return unixStartTime
+    else:
+        return 0
+
+
+def endTime(studentID):
+    if exists(studentID):
+        y = str(merged_data.at[studentID, 'Leave Time'])
+        x = str(date.datetime.now().date()) + "T" + y[11:16] + ":00"
+        z = date.datetime.fromisoformat(x)
+        unixEndTime = int(time.mktime(z.timetuple()))
+        return unixEndTime
+    else:
+        return 0
